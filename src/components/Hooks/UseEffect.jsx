81c382d2145f8b789d104b498fe8c2f4c5b20512
userGithun <chacha45454@gmail.com>
2025-04-07 17:04:19 +0530

    Refactor UseEffect component to include state management and API fetching

--- src/components/Hooks/UseEffect.jsx
+++ src/components/Hooks/UseEffect.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function UseEffect() {
+
+    const [count, setCount]= useState(0)
+    const [data, setData]  = useState(100)
+
+    const getApi =() =>{
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+             .then(response => response.json())
+             .then(json => console.log(json))
+    }
+
+    useEffect(()=>{
+        getApi()
+        
+        console.log('hello render useEffer')
+    },[])
+
   return (
-    <div>UseEffect</div>
+    <>
+    <h1>You clicked {count} Time's</h1>
+    <h1>You Clicked {data} Time's</h1>
+    <br /><br />
+    <button onClick={()=>{setCount(count + 1)}}>Click me</button>
+    &nbsp; &nbsp;
+    <button onClick={()=>{setData(data + 1)}}>Click me</button>
+    </>
   )
 }
 
